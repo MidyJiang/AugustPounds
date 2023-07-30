33d35664c635ced85fa30e588e20ec45a2791313
Midy <70130392+MidyJiang@users.noreply.github.com>
2023-07-30 19:47:30 +0800

    Update walkpath.py

--- walkpath.py
+++ walkpath.py
@@ -1,9 +1,15 @@
 import os
 
-for a,b,c in os.walk(os.path.abspath('')):
-    print(c)
+def list_files(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.abspath(os.path.join(root, file))
+            print(file_path)
+
+# 调用函数，传入要遍历的目录路径
+list_files(os.path.abspath(""))
 
 
 import pandas as pd
-df=pd.read_csv("send.csv")
+df=pd.read_csv(r"My Artifact/send.csv")
 print(df)
