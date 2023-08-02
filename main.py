cb6532aa3442af8f1675b9eb07b2d7e58921de0f
Midy <70130392+MidyJiang@users.noreply.github.com>
2023-08-02 21:51:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ start=time.time()
 warnings.filterwarnings('ignore')
 
 
-plt.rcParams["font.sans-serif"]=["SimHei"] #设置字体
-plt.rcParams["axes.unicode_minus"]=False #该语句解决图像中的“-”负号的乱码问题
+# plt.rcParams["font.sans-serif"]=["SimHei"] #设置字体
+# plt.rcParams["axes.unicode_minus"]=False #该语句解决图像中的“-”负号的乱码问题
 
 
 url=r'https://www.boc.cn/sourcedb/whpj/'
